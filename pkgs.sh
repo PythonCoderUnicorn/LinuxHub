ac3847c6bc1e23ec35657e6f5d37d36062b96705
Zane (She/They) <55933131+PythonCoderUnicorn@users.noreply.github.com>
2024-07-13 17:14:18 -0600

    Create pkgs.sh

---
+++ pkgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+
+sudo apt install figlet \
+lolcat \
+gedit \
+neovim \
+tmux \
+tree \
+
+#--- flatpaks 
+flatpak install flathub dev.geopjr.Calligraphy
+flatpak install flathub md.obsidian.Obsidian
